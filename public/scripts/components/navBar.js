8b9d3444f907d9a10a848b3e26a9caa0da3285e2
sachieko <arashikabane@gmail.com>
2022-08-30 15:57:08 -0600

    Selectors change, fix syntax error in html, menu links to category

--- public/scripts/components/navBar.js
+++ public/scripts/components/navBar.js
@@ -14,13 +14,13 @@ $(() => {
       </nav>
       <header class="menu-links">
       <div>
-        <a href="/" class="menu-link">Appetizers</a>
-        <a href="/" class="menu-link">Salads</a>
-        <a href="/" class="menu-link">Potato</a>
-        <a href="/" class="menu-link">Mains</a>
-        <a href="/" class="menu-link">Desserts</a>
-        <a href="/" class="menu-link">Drinks</a>
-        <a href="/" class="menu-link">Sauces</a>
+        <a href="#Appetizers" class="menu-link">Appetizers</a>
+        <a href="#Salads" class="menu-link">Salads</a>
+        <a href="#Potato" class="menu-link">Potato</a>
+        <a href="#Mains" class="menu-link">Mains</a>
+        <a href="#Desserts" class="menu-link">Desserts</a>
+        <a href="#Drinks" class="menu-link">Drinks</a>
+        <a href="#Sauces" class="menu-link">Sauces</a>
       </div>
     </header>`
   );
@@ -30,11 +30,11 @@ $(() => {
     viewsManager.show('app');
   });
 
-  $('body').on('click', 'header #navOrdersButton', function() {
+  $('body').on('click', 'nav #navOrdersButton', function() {
     viewsManager.show('orders');
   });
 
-  $('body').on('click', 'header #navCheckoutButton', function() {
+  $('body').on('click', 'nav #cart-btn', function() {
     getCart().then(cart => {
       viewsManager.show('checkout');
       const $checkoutItem = $checkoutPage.find('#checkoutItems');
@@ -44,7 +44,7 @@ $(() => {
             <td>${product.item.name}</td>
             <td>$${product.item.price} x ${product.count} = $${product.item.price * product.count}</td>
             <td>
-              <form class=removeItem>
+              <form class="removeItem">
                 <input type="hidden" name="id" value="${product.item.id}"></input>
                 <button>Remove</button>
               </form>
