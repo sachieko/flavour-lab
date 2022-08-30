4433758a9ea2c040da31738f96649ca7e86df4d5
sachieko <arashikabane@gmail.com>
2022-08-30 14:44:26 -0600

    Header and nav combine

--- public/scripts/components/navBar.js
+++ public/scripts/components/navBar.js
@@ -1,10 +1,27 @@
 /* eslint-disable no-undef */
 $(() => {
   const $navBar = $(
-    `<header>
+    `
+      <nav>
+      <div>
       <img style="width:50px" src="../../images/fieri.png" />
+      <span class="logo">Flavour Labs</span>
+      </div>
+      <div>
       <button id="navOrdersButton">Orders</button>
-      <button id="navCheckoutButton">Cart</button>
+      <button type="button" id="cart-btn"><i class="fa fa-shopping-cart"> Cart</i></button>
+      </div>
+      </nav>
+      <header class="menu-links">
+      <div>
+        <a href="/" class="menu-link">Appetizers</a>
+        <a href="/" class="menu-link">Salads</a>
+        <a href="/" class="menu-link">Potato</a>
+        <a href="/" class="menu-link">Mains</a>
+        <a href="/" class="menu-link">Desserts</a>
+        <a href="/" class="menu-link">Drinks</a>
+        <a href="/" class="menu-link">Sauces</a>
+      </div>
     </header>`
   );
   window.$navBar = $navBar;
@@ -19,3 +36,25 @@ $(() => {
     viewsManager.show('checkout');
   });
 });
+/*
+<!-- Top nav bar (fixed) -->
+  <nav>
+    <span class="logo">Flavour Labs</span>
+    <div class="cart">
+
+    </div>
+  </nav>
+
+  <!-- Header menu links (fixed) -->
+  <header class="menu-links">
+    <div>
+      <a href="/" class="menu-link">Appetizers</a>
+      <a href="/" class="menu-link">Salads</a>
+      <a href="/" class="menu-link">Potato</a>
+      <a href="/" class="menu-link">Mains</a>
+      <a href="/" class="menu-link">Desserts</a>
+      <a href="/" class="menu-link">Drinks</a>
+      <a href="/" class="menu-link">Sauces</a>
+    </div>
+  </header>
+  */
