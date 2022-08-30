35cf65ef3dff69b2c8676ec949dafa2e702fd239
sachieko <arashikabane@gmail.com>
2022-08-30 17:18:56 -0600

    Returns order from database after insert

--- db/queries/order_details.js
+++ db/queries/order_details.js
@@ -4,7 +4,8 @@ const insertOrderDetails = (details) => {
   console.log(details);
   return db.query(`
   INSERT INTO order_details (order_id, item_id, quantity, price, note)
-  VALUES ($1, $2, $3, $4, $5)`,
+  VALUES ($1, $2, $3, $4, $5)
+  RETURNING *;`,
   [details.order_id, details.item_id, details.quantity, details.price, details.note])
     .then(data => {
       return data.rows[0];
