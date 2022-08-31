43d302264ca203e9c0fec56b9caf90f2081d0f28
sachieko <arashikabane@gmail.com>
2022-08-30 18:35:44 -0600

    Posts orders but has major security flaw

--- public/scripts/components/checkout.js
+++ public/scripts/components/checkout.js
@@ -26,23 +26,39 @@ $(() => {
     event.preventDefault();
     const data = $(this).serialize();
     removeFromCart(data).then(res => {
-      $('body').find('header #navCheckoutButton').trigger('click');
+      $('body').find('nav #cart-btn').trigger('click');
     });
   });
 
-  $checkoutPage.on('submit', '#checkoutForm', function(event) {
+  const makeFormObject = function(array) {
+    const returnObj = {};
+    for (const keyValuePair of array) {
+      returnObj[keyValuePair.name] = keyValuePair.value;
+    }
+    return returnObj;
+  };
+
+  $('body').on('submit', '#checkoutForm', function(event) { // click for testing purposes, submit for actual
     event.preventDefault();
-    const data = $(this).serialize();
-    sendText(data).then((order)=>{
-      for (const product of order) {
-        $myOrders.append(
-          `<tr>
-            <td>${product.name}</td>
-            <td>$${product.price}</td>
-          </tr>`);
-      }
-      viewsManager.show('orders');
+    const order = makeFormObject($(this).serializeArray()); // serialize Array output: [{ name:formname, value: formvalue }...]
+    console.log('data from form:', order);
+    getCart().then(orderDetails => {
+      console.log('cart: ', orderDetails);
+      addOrder(order)
+        .then(orderId => {
+          for (const orderDetail of orderDetails) {
+            const detailObj = {};
+            detailObj.order_id = Number(orderId);
+            detailObj.item_id = orderDetail.item.id;
+            detailObj.quantity = orderDetail.count;
+            detailObj.price = orderDetail.item.price;
+            detailObj.note = orderDetail.item.note;
+            addOrderDetails(detailObj);
+          }
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
     });
   });
-
 });
