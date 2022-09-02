d7ffba2990fde1ced76e7456f823f83739752fb5
sachieko <arashikabane@gmail.com>
2022-09-02 03:49:12 -0600

    Restyle buttons, remove console log

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -26,7 +26,7 @@ $(() => {
     item.is_available ? $($itemHTML).children('div.item-info').append(`
     <form class="view-item-btn">
     <input type="hidden" name="itemId" value="${item.id}">
-    <button type="submit" class="add-btn primary-btn">Add to Order</button>
+    <button type="submit" class="add-btn primary-btn">+</button>
     </form>`) : $($itemHTML).children('div.item-info').append(`
     <span class="unavailable">Item is unavailable</span>
     `);
