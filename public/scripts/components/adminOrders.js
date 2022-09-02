a7b3ee6d672738992a3350cdae97d1b9c6609be6
sylvielcq <slcq.dev@gmail.com>
2022-09-02 01:18:56 -0700

    Fix indentation on orders.js

--- public/scripts/components/adminOrders.js
+++ public/scripts/components/adminOrders.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-undef */
 $(() => {
-  const $adminOrderPage = $(
-    `<div id="adminOrders"">
-        <h1>Customer Orders</h1>
-        <div class="customerOrders">
-        </div>
-    </div>`
-  );
+  const $adminOrderPage = $(`
+    <div id="adminOrders"">
+      <h1>Customer Orders</h1>
+      <div class="customerOrders"></div>
+    </div>
+  `);
+
   window.$adminOrderPage = $adminOrderPage;
 
   const letsPlay = function() {
@@ -15,29 +15,30 @@ $(() => {
         $navBar.detach();
         $adminNav.prependTo('body');
         $adminOrderPage.append(`<div class="customerOrders"></div>`);
+
         let j = 0;
         const $orderTable = $adminOrderPage.find('.customerOrders').last();
         for (let i = 0; i < orders.length; i = j) {
           $orderTable.append(`<div class="customerOrderBox"></div>`);
           let order = orders[i];
-          $orderTable.find(".customerOrderBox").last().append(
-            `<div class="metaOrderInfo">
-                <div class="customerContactInfo">
-                  <h5>Contact:</h5>
-                  <span>${order.name}</span>
-                  <span>${order.phone}</span>
-                </div>
-                <div class="orderStatus">
-                  <h5>Status:</h5>
-                  <p>${order.completed_time ? `Completed at ${order.completed_time}` : `Outstanding Order `}</p>
-                  <p>${order.estimated_time ? `Expected completion ${order.estimated_time}` : 'Awaiting ETA'}</p>
-                  <p>Submitted At ${order.submit_time}</p>
-                </div>
+          $orderTable.find(".customerOrderBox").last().append(`
+            <div class="metaOrderInfo">
+              <div class="customerContactInfo">
+                <h5>Contact:</h5>
+                <span>${order.name}</span>
+                <span>${order.phone}</span>
               </div>
-              ${!order.completed_time ?
-                `<div class="timeManagement">
-                  <h5>Manage Order</h5>
-                  <div id="commands">
+              <div class="orderStatus">
+                <h5>Status:</h5>
+                <p>${order.completed_time ? `Completed at ${order.completed_time}` : `Outstanding Order `}</p>
+                <p>${order.estimated_time ? `Expected completion ${order.estimated_time}` : 'Awaiting ETA'}</p>
+                <p>Submitted At ${order.submit_time}</p>
+              </div>
+            </div>
+            ${!order.completed_time ? `
+            <div class="timeManagement">
+              <h5>Manage Order</h5>
+                <div id="commands">
                   <form class="startOrder">
                     <input type="hidden" value="Start" name="cmd"/>
                     <input type="hidden" value=${order.id} name="id"/>
@@ -47,8 +48,8 @@ $(() => {
                     <input type="hidden" value="Estimate" name="cmd"/>
                     <input type="hidden" value=${order.id} name="id"/>
                     <div id="est-form-btn">
-                    <input type="number" name="est" id="number-input" placeholder="Minutes"/>
-                    <button id="btn-est">2. Estimated Prep Time</button>
+                      <input type="number" name="est" id="number-input" placeholder="Minutes"/>
+                      <button id="btn-est">2. Estimated Prep Time</button>
                     </div>
                   </form>
                   <form class="completeOrder">
@@ -56,32 +57,34 @@ $(() => {
                     <input type="hidden" value=${order.id} name="id"/>
                     <button id="btn-complete">3. Completed</button>
                   </form>
-                  </div>
-                </div>` : ''}
-                <div class="toggleItems" id="items-toggle">
+                </div>
+              </div>` : ''}
+              <div class="toggleItems" id="items-toggle">
                 <h5 class="toggleItems">Order Items</h5>
                 <button type="button" class="toggleItems"><i class="fa-solid fa-arrow-down-wide-short"></i></i></button>
-                </div>
-                <div class="customerItems">
-                </div>
+              </div>
+              <div class="customerItems">
+              </div>
               </div>`);
           j = i;
           while (order.id === orders[j].id) {
-            $orderTable.find(".customerOrderBox").last().find(".customerItems").append(
-              `<div class="customerItem">
+            $orderTable.find(".customerOrderBox").last().find(".customerItems").append(`
+              <div class="customerItem">
                   <span id="item-name">${orders[j].item_name}</span>
                   <span>x ${orders[j].item_quantity}</span>
                   <span >$${orders[j].item_price}</span>
-                </div>`);
+              </div>`);
             j++;
             if (j >= orders.length - 1) {
               break;
             }
           }
         }
+
         $adminOrderPage.append($orderTable);
         $adminOrderPage.find('.customerOrders').first().remove();
       })
+
       .catch(res => {
         //youre a fake admin, poser
       });
